bbf0b1523fe8d0cc2957689e28d6da18b3c23290
Elldarm1 <emizrachi1@knights.ucf.edu>
2023-06-16 20:03:30 -0400

    this is commit

--- lab_assignment_5.c
+++ lab_assignment_5.c
@@ -1,76 +1,103 @@
+
 #include <stdio.h>
 #include <stdlib.h>
-
-typedef struct node {
-	char letter;
-	struct node* next;
+    typedef struct node
+{
+    char letter;
+    struct node *next;
 } node;
-
 // Returns number of nodes in the linkedList.
-int length(node* head)
+int length(node *head)
 {
-   struct node *tmp = head;
-    int len = 0;
-   while (tmp != NULL)
-   {
-      tmp = tmp->next;
-      len++;
-   }
+    int count = 0;
+    if(head == NULL) return 0;
+    
+    while(head != NULL){
+        head = head->next;
+        count++;
+    }
 
-   return (len);
+    return count;
+    
 }
-
 // parses the string in the linkedList
-//  if the linked list is head -> |a|->|b|->|c|
-//  then toCString function wil return "abc"
-char* toCString(node* head)
+// if the linked list is head -> |a|->|b|->|c|
+// then toCString function wil return "abc"
+char *toCString(node *head)
 {
-}
+    node* temp = head;
+    if(temp == NULL) return NULL;
+    int len = length(head);
+    char* result = (char*)malloc((len+1)*sizeof(char));
+    for(int i = 0; i < len; i++){
+        result[i] = temp->letter;
+        temp = temp->next;
+    }
+    result[len] = '\0';
+    return result;
 
+}
 // inserts character to the linkedlist
 // f the linked list is head -> |a|->|b|->|c|
 // then insertChar(&head, 'x') will update the linked list as foolows:
 // head -> |a|->|b|->|c|->|x|
-void insertChar(node** pHead, char c)
+void insertChar(node **pHead, char c)
 {
-}
+    node* new_node = malloc(sizeof(node));
+    new_node->letter = c;
+    new_node->next = NULL;
+     
+    node* temp = *pHead;
 
+    if(*pHead == NULL){
+        *pHead = new_node;
+        return;
+    }
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = new_node;
+    
+}
 // deletes all nodes in the linkedList.
-void deleteList(node** pHead)
+void deleteList(node **pHead)
 {
-}
+    node* temp = *pHead;
+    node* ptr;
 
+    while(temp->next != NULL){
+        ptr = temp->next;
+        free(temp);
+        temp = ptr;
+    }
+    *pHead = NULL;
+
+}
 int main(void)
 {
-	int i, strLen, numInputs;
-	node* head = NULL;
-	char* str;
-	char c;
-	FILE* inFile = fopen("input.txt","r");
-
-	fscanf(inFile, " %d\n", &numInputs);
-	
-	while (numInputs-- > 0)
-	{
-		fscanf(inFile, " %d\n", &strLen);
-		for (i = 0; i < strLen; i++)
-		{
-			fscanf(inFile," %c", &c);
-			insertChar(&head, c);
-		}
-
-		str = toCString(head);
-		printf("String is : %s\n", str);
-
-		free(str);
-		deleteList(&head);
-
-		if (head != NULL)
-		{
-			printf("deleteList is not correct!");
-			break;
-		}
-	}
-
-	fclose(inFile);
+    int i, strLen, numInputs;
+    node *head = NULL;
+    char *str;
+    char c;
+    FILE *inFile = fopen("input.txt", "r");
+    fscanf(inFile, " %d\n", &numInputs);
+    while (numInputs-- > 0)
+    {
+        fscanf(inFile, " %d\n", &strLen);
+        for (i = 0; i < strLen; i++)
+        {
+            fscanf(inFile, " %c", &c);
+            insertChar(&head, c);
+        }
+        str = toCString(head);
+        printf("String is : %s\n", str);
+        free(str);
+        deleteList(&head);
+        if (head != NULL)
+        {
+            printf("deleteList is not correct!");
+            break;
+        }
+    }
+    fclose(inFile);
 }
